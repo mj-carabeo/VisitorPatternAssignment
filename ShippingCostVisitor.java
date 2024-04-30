7c5469f5e5b33e426f898754f4122c6d52594f13
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-04-30 19:43:04 +0800

    Update ShippingCostVisitor.java

--- ShippingCostVisitor.java
+++ ShippingCostVisitor.java
@@ -1,11 +1,13 @@
 public interface ShippingCostVisitor {
     void visit(Chair chair);
     void visit(Sofa sofa);
+    void visit(Table table);
 }
 
 public class FlatRateShippingCostVisitor implements ShippingCostVisitor {
     private static final double CHAIR_SHIPPING_COST = 10.0;
     private static final double SOFA_SHIPPING_COST = 20.0;
+    private static final double TABLE_SHIPPING_COST = 30.0;
 
     @Override
     public void visit(Chair chair) {
@@ -16,6 +18,10 @@ public class FlatRateShippingCostVisitor implements ShippingCostVisitor {
     public void visit(Sofa sofa) {
         System.out.println("Sofa shipping cost: " + SOFA_SHIPPING_COST);
     }
+
+    @Override
+    public void visit(Table table) {
+        System.out.println("Table shipping cost: " + TABLE_SHIPPING_COST);
 }
 
 public class DistanceBasedShippingCostVisitor implements ShippingCostVisitor {
@@ -37,5 +43,11 @@ public class DistanceBasedShippingCostVisitor implements ShippingCostVisitor {
     public void visit(Sofa sofa) {
         double shippingCost = BASE_RATE + 2 * RATE_PER_MILE * distance;
         System.out.println("Sofa shipping cost: " + shippingCost);
+    }
+
+    @Override
+    public void visit(Table table) {
+        double shippingCost = BASE_RATE + 2 * RATE_PER_MILE * distance;
+        System.out.println("Table shipping cost: " + shippingCost);
     }
 }
