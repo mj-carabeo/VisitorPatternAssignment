842b0824176617864d18a3a786f49bbfad87a935
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-04-30 19:36:42 +0800

    Create Furniture.java

---
+++ Furniture.java
@@ -0,0 +1,17 @@
+public interface Furniture {
+    void accept(ShippingCostVisitor visitor);
+}
+
+public class Chair implements Furniture {
+    @Override
+    public void accept(ShippingCostVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+public class Sofa implements Furniture {
+    @Override
+    public void accept(ShippingCostVisitor visitor) {
+        visitor.visit(this);
+    }
+}
